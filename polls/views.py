3535205964bb3e7b17ad4fe0529540bdc254c777
Diego Saavedra <dsaavedra88@gmail.com>
2022-12-06 17:56:51 -0500

    Part 3: Views

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola mundo!")
+    return HttpResponse("Estás en la página principal de Encuestas")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás viendo a la pregunta número {question_id}")
 
